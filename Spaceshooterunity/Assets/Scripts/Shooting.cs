23a5d7b93aa24e4e56cce39f4764bf17a638b183
junyuchang <91118598+junyuchang@users.noreply.github.com>
2023-06-27 18:25:19 -0400

    Added Cone and Line upgrades
    adds two extra guns depending on the upgrade chosen (under MainScene)

--- Spaceshooterunity/Assets/Scripts/Shooting.cs
+++ Spaceshooterunity/Assets/Scripts/Shooting.cs
@@ -1,4 +1,5 @@
 //Taken from https://www.youtube.com/watch?v=LNLVOjbrQj4&ab_channel=Brackeys
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,11 +10,15 @@ public class Shooting : MonoBehaviour
     public GameObject bulletPreFab;
 
     public float bulletForce = 50f;
-    public float firerate = 3f;
+    public float firerate = 4f;
+
     private bool isShooting = false;
     private Coroutine shooting;
     private bool toggleOn = false;
-    private float waitTime;
+
+    //upgrades (maybe move to different script like stats or something)
+    public int fireRateLvl = 1, coneLvl = 1, lineLvl = 1;
+
     // Update is called once per frame
     void Update()
     {
@@ -54,14 +59,107 @@ public class Shooting : MonoBehaviour
         }
     }
 
+
     void shoot(){
+        
         GameObject bullet = Instantiate(bulletPreFab, firePoint.position,firePoint.rotation);
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
         rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+        if (coneLvl > 1)
+        {
+            extraCones(coneAngles());
+        }
+        if (lineLvl > 1)
+        {
+            extraLines(linePos());
+        }
     }
+
+    // below are helpers for extra cone and line guns from upgrades
+
+    void extraLines(List<float> pos)
+    {
+        for (int i = 0; i < pos.Count; i++)
+        {
+            Vector2 offsetVector = Quaternion.Euler(0, 0, 90) * firePoint.up;
+            offsetVector *= pos[i];
+            Vector3 offsetPosition = firePoint.position + (Vector3)offsetVector;
+
+            GameObject bullet = Instantiate(bulletPreFab, offsetPosition, firePoint.rotation);
+            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+            rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+        }
+    }
+
+    List<float> linePos()
+    {
+        List<float> pos = new List<float>();
+        float interval = 0.25f / (lineLvl - 1);
+        for (int i = 1; i < lineLvl; i++)
+        {
+            pos.Add(i * interval);
+            pos.Add(-i * interval);
+        }
+        return pos;
+    }
+
+
+    void extraCones(List<int> angles)
+    {
+        for (int i = 0; i < angles.Count; i++)
+        {
+            Vector2 direction = Quaternion.Euler(0, 0, angles[i]) * firePoint.up;
+            GameObject bullet = Instantiate(bulletPreFab, firePoint.position, Quaternion.Euler(0, 0, angles[i]) * firePoint.rotation);
+            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+            rb.AddForce(direction * bulletForce, ForceMode2D.Impulse);
+        }
+    }
+
+    List<int> coneAngles()
+    {
+        List<int> angles = new List<int> ();
+        int interval = 1;
+        if (2 <= coneLvl && coneLvl <= 3)
+        {
+            interval = (int) Math.Round(20f / (coneLvl - 1));
+        }
+        else if (4 <= coneLvl && coneLvl <= 6)
+        {
+            interval = (int) Math.Round(45f / (coneLvl - 1));
+        }
+        else if (7 <= coneLvl && coneLvl <= 9)
+        {
+            interval = (int) Math.Round(60f / (coneLvl - 1));
+        }
+        else if (coneLvl > 9)
+        {
+            interval = (int)Math.Round(75f / (coneLvl - 1));
+        }
+
+        for (int i = 1; i < coneLvl; i++)
+        {
+            angles.Add(i * interval);
+            angles.Add(-i * interval);
+        }
+
+        return angles;
+    }
+
+
+    // Below are upgrades
 
     public void FireRateUp()
     {
         firerate = firerate * 1.75f;
+    }
+
+    public void ConeLvlUp()
+    {
+        coneLvl += 1;
+    }
+
+    public void LineLvlUp()
+    {
+        lineLvl += 1;
     }
 }
